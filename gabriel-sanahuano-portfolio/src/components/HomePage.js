624792255afececeafe6df45aee5a828f877132b
githubGaboDotCom <githubgabodotcom@gmail.com>
2023-02-28 23:46:58 -0700

    working in adding background music to homepage

--- gabriel-sanahuano-portfolio/src/components/HomePage.js
+++ gabriel-sanahuano-portfolio/src/components/HomePage.js
@@ -4,6 +4,10 @@ function HomePage () {
     return (
         <div className="App">
             <video src="/backgroundVideoNightlyEarth.mp4" autoPlay loop muted />
+            <audio controls autoPlay loop > 
+                <source src= "/backgroundElectroMusic.mp3" type="audio/mpeg" />
+            </audio>
+            {/* <embed src="/backgroundElectroMusic.mp3" loop="true" autostart="true" width="2" height="0"></embed> */}
         </div>
     );
 }
