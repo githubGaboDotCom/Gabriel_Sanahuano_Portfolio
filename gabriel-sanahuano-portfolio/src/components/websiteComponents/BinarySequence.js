75e6a56e69c2b221abbc4524dc061429944d7819
githubgabodotcom <githubgabodotcom@gmail.com>
2023-09-24 01:05:29 -0600

    Practicing with the framer motion library

--- gabriel-sanahuano-portfolio/src/components/websiteComponents/BinarySequence.js
+++ gabriel-sanahuano-portfolio/src/components/websiteComponents/BinarySequence.js
@@ -3,10 +3,11 @@ import { useRef } from 'react';
 import {wrap} from '@motionone/utils';
 import '../styles/css/BinarySequence.css';
 
-function MotionedNumberSequence (children, baseVelocity = 100) { 
+function MotionedNumberSequence ({children}, baseVelocity) { 
 
     const baseX = useMotionValue(0);
     const { scrollY } = useScroll();
+    // const x = useTransform(scrollYProgress, [0, 1], [0, 1000]);
     const scrollVelocity = useVelocity(scrollY);
     const smoothVelocity = useSpring(scrollVelocity, {
         damping: 50,
@@ -15,37 +16,40 @@ function MotionedNumberSequence (children, baseVelocity = 100) {
     const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], {
         clamp: false
     });
-
-    const x = useTransform(baseX, (v) => `${wrap(-20, -45, v)}%`);
-
-    const directionFactor = useRef(1);
-    useAnimationFrame((t, delta) => {
-        let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
+    const x = useTransform(baseX, (v) => `${wrap(-43, -45, v)}%`);
+    // const directionFactor = useRef(1);
+    // useAnimationFrame((t, delta) => {
+    //     let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
         
-        if (velocityFactor.get() < 0) {
-        directionFactor.current = -1;
-        } else if (velocityFactor.get() > 0) {
-        directionFactor.current = 1;
-        }
+    //     if (velocityFactor.get() < 0) {
+    //     directionFactor.current = -1;
+    //     } else if (velocityFactor.get() > 0) {
+    //     directionFactor.current = 1;
+    //     }
 
-        moveBy += directionFactor.current * moveBy * velocityFactor.get();
+    //     moveBy += directionFactor.current * moveBy * velocityFactor.get();
 
-        baseX.set(baseX.get() + moveBy);
-    });
+    //     baseX.set(baseX.get() + moveBy);
+    // });
 
     return (
         <div className = 'NumbersContainer'>
             <motion.div className = 'numbers' style={{ x }}>
-                <span>{children.numbersSequence}</span>
+                <span>{children}</span>
+                <span>{children}</span>
+                <span>{children}</span>
+                <span>{children}</span>
             </motion.div>
         </div>
     );
 }
 
 function BinarySequence () {
+
     return (
         <section>
-            <MotionedNumberSequence numbersSequence = {'1 0 1 0 1 0'} baseVelocity = {-5} />
+            <MotionedNumberSequence baseVelocity = {-5}>1 0 1 0 1 0</MotionedNumberSequence>
+            <MotionedNumberSequence baseVelocity = {-5}>1 0 1 0 1 0</MotionedNumberSequence>
         </section>
     );
 }
