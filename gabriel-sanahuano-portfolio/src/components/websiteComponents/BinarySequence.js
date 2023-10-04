ea266f3233eaec44aa5301d9a6c66e3fca747275
githubGaboDotCom <githubgabodotcom@gmail.com>
2023-10-03 23:55:32 -0600

    Working in the animation for the Binary Sequence. It only needs to run once

--- gabriel-sanahuano-portfolio/src/components/websiteComponents/BinarySequence.js
+++ gabriel-sanahuano-portfolio/src/components/websiteComponents/BinarySequence.js
@@ -1,5 +1,6 @@
 import {useMotionValue, useTransform, motion, useAnimationFrame} from 'framer-motion';
 import {wrap} from '@motionone/utils';
+import { useEffect, useRef } from 'react';
 import '../styles/css/BinarySequence.css';
 
 function BinarySequenceToTheLeft ({children}) { 
@@ -8,7 +9,6 @@ function BinarySequenceToTheLeft ({children}) {
     const x = useTransform(baseX, (v) => `${wrap(-105, 80, v)}%`);
     useAnimationFrame((t, delta) => {
         let moveBy = -9 * (delta / 1000);
-        //Making a comment
 
         baseX.set(baseX.get() + moveBy);
     });
@@ -35,11 +35,32 @@ function BinarySequenceToTheRight ({children}) {
 
     const baseX = useMotionValue(95);
     const x = useTransform(baseX, (v) => `${wrap(-100, 100, v)}%`);
-    useAnimationFrame((t, delta) => {
-        let moveBy = 9 * (delta / 1000);
+    let initial = new Date().getTime();
+    const last = useRef(initial);
+    let moveBy = useRef(0);
+    baseX.set(baseX.get() + moveBy);
+    
+    // useAnimationFrame((t, delta) => {
+    //     console.log(delta);
+    //     const now = new Date().getTime();
+    //     let deltaTime = now - last;
+    //     last = now;
+    //     let moveBy = 9 * (deltaTime / 1000);
+    //     console.log(moveBy);
+    //     baseX.set(baseX.get() + moveBy);
 
-        baseX.set(baseX.get() + moveBy);
-    });
+    // });
+
+    useEffect(() => {
+        const now = new Date().getTime();
+        let deltaTime = now - last.current;
+        last.current = now;
+        console.log(deltaTime);
+        let moveUpdate = 9 * (deltaTime / 1000);
+        moveBy.current = moveUpdate;
+        // console.log(moveBy);
+        // baseX.set(baseX.get() + moveBy);
+    }, []);
 
     return (
         <div className = 'NumbersContainer'>
