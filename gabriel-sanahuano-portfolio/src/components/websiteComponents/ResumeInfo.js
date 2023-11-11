e72af8bb4d5cf39c231aa654e11adcf15be3fea3
githubGaboDotCom <githubgabodotcom@gmail.com>
2023-11-10 23:59:14 -0700

    Updating the ResumeInfo.js file

--- gabriel-sanahuano-portfolio/src/components/websiteComponents/ResumeInfo.js
+++ gabriel-sanahuano-portfolio/src/components/websiteComponents/ResumeInfo.js
@@ -1,7 +1,13 @@
 import { motion } from 'framer-motion';
 import '../styles/css/ResumeInfo.css';
+import { Slider } from './Slider';
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function ResumeInfo () {
+
+    const [current, setCurrent] = useState(0);
+    const resumeSlider = Slider.length;
 
     return (
         <div className="ResumeParentContainer">
@@ -11,6 +17,10 @@ function ResumeInfo () {
             transition = {{duration: 0.5, delay: 0.5, ease: [0, 0.71, 0.2, 1.01]}}>
                 <div className='TitleResumeContainer'>
                     <h2>Gabriel A. Sanahuano</h2>
+                </div>
+                <div>
+                    <FontAwesomeIcon icon="fa-solid fa-circle-arrow-left" />
+                    <FontAwesomeIcon icon="fa-solid fa-circle-arrow-right" />
                 </div>
             </motion.div>
         </div>
--- gabriel-sanahuano-portfolio/src/components/websiteComponents/Slider.js
+++ gabriel-sanahuano-portfolio/src/components/websiteComponents/Slider.js
@@ -5,7 +5,14 @@ export const Slider = [
       Date: "Jan 2019 - Dec 2022",
       University: "Brigham Young University - Idaho",
       Location: "Rexburg, Idaho, United States",
-      Coursework: "Introduction to Software Development, Object-Oriented Software Development, Fundamentals of Digital Systems,  Web Engineering I, Data Structures, Technical Documentation, Database Design & Development, Software Design & Development, Web Engineering II, Software Engineering III, Comp. Communication & Networks, Networking, Systems Security I, Discrete Mathematics I, Computer Security, Parallelism and Concurrency, Algorithms & Complexity, Introduction to Robotics, Clojure Language.",
+      Coursework: "Introduction to Software Development, Object-Oriented Software Development, " +
+      "Fundamentals of Digital Systems,  Web Engineering I, Data Structures, Technical Documentation, " +
+      "Database Design & Development, Software Design & Development, Web Engineering II, " +
+      "Data Science Programming, Software Engineering III, Comp. Communication & Networks, " +
+      "Networking, Systems Security I, Discrete Mathematics I, Machine Learning and Data Mining, " +
+      "Big Data Programming and Analytics, Computer Security, Parallelism and Concurrency, Algorithms " +
+      "& Complexity, Introduction to Robotics, Clojure Language, Linear Algebra, Applied Calculus for " +
+      "Data Analysis."
     },
     {
       EducationTitle: "Education",
@@ -13,7 +20,19 @@ export const Slider = [
       Date: "Jul 2021 - Jul 2022",
       University: "Brigham Young University - Idaho",
       Location: "Rexburg, Idaho, United States",
-      Coursework: "Data Science Programming, Machine Learning and Data Mining, Big Data Programming and Analytics, Linear Algebra, Applied Calculus for Data Analysis.",
+      FirstOutcome: "Use functions, data structures, and other programming constructs efficiently to process data.",
+      SecondOutcome: "Load data from various types of data sources, including files, databases, and remote services.",
+      ThirdOutcome: "Use data science libraries to perform analysis, produce charts, and prepare data for machine " +
+      "learning algorithms.",
+      FourthOutcome: "Explain the approaches and appropriate applications of a broad set of machine learning algorithms.",
+      FifthOutcome: "Integrate and extend previously learned data science tools to analyze remote and distributed data " +
+      "in business contexts.",
+      SixthOutcome: "Use mathematical software and/or calculators to solve a variety of applications in the Physical " +
+      "Sciences, Computer Science, or Economics.",
+      SeventhOutcome: "Gain proficiency in solving systems of linear equations using matrices, and demonstrate a working " +
+      "knowledge of the algebraic properties of matrices.",
+      EigthOutcome: "Acquire facility working with general vector spaces, linear transformations, coordinate vectors, " +
+      "and the changing of bases"
     },
     {
        ExperienceTitle: "Work Experience",
@@ -21,12 +40,19 @@ export const Slider = [
        Date: "Jan 2023 - Present",
        Company: "The Church of Jesus Christ of Latter-day Saints",
        Location: "Riverton, Utah, United States",
-       FirstJobDetail: "Contributed to over 170 user stories in Azure DevOps by conducting regression testing, analyzing results, and submitting observations to the development team.",
-       SecondJobDetail: "Participated in design reviews to identify inconsistencies//issues in product requirements by interacting with business analysts and developers during the sprint cycle.",
-       ThirdJobDetail: "Performed integration, End-to-End, performance, regression, sanity, and smoke testing on Mobile and Web applications by developing negative and positive test cases according to business requirements.",
-       FourthJobDetail: "Created automated test cases using Cypress and executed SQL queries for data validation and debugging.",
-       FifthJobDetail: "Assisted with continuous integration and deployment practices in Azure DevOps using an Agile Software Development process.",
-       Skills: "Software Testing Life Cycle (STLC) · QA Automation · Test Scripts · JavaScript · SQL · Microsoft Dynamics 365 · Azure DevOps · Git · Visual Studio IDE · Cypress · Agile Software Development."    
+       FirstJobDetail: "Contributed to over 170 user stories in Azure DevOps by conducting regression " +
+       "testing, analyzing results, and submitting observations to the development team.",
+       SecondJobDetail: "Participated in design reviews to identify inconsistencies//issues in product " +
+       "requirements by interacting with business analysts and developers during the sprint cycle.",
+       ThirdJobDetail: "Performed integration, End-to-End, performance, regression, sanity, and smoke " +
+       "testing on Mobile and Web applications by developing negative and positive test cases according " +
+       "to business requirements.",
+       FourthJobDetail: "Created automated test cases using Cypress and executed SQL queries for data " +
+       "validation and debugging.",
+       FifthJobDetail: "Assisted with continuous integration and deployment practices in Azure DevOps " +
+       "using an Agile Software Development process.",
+       Skills: "Software Testing Life Cycle (STLC) · QA Automation · Test Scripts · JavaScript · SQL · " + 
+       "Microsoft Dynamics 365 · Azure DevOps · Git · Visual Studio IDE · Cypress · Agile Software Development."    
     },
     {
         ExperienceTitle: "Work Experience",
@@ -35,8 +61,10 @@ export const Slider = [
         Company: "We Care Insurance",
         Location: "Omaha, Nebraska, United States",
         FirstJobDetail: "Designed, created, and executed automation scripts using Java and Selenium.",
-        SecondJobDetail: "Configured test environments for running test cases and scenarios using Spring Tool Suite. Assisted with software testing",
-        ThirdJobDetail: "Participated in the deployment of QA processes using software tools and supporting technologies.",
+        SecondJobDetail: "Configured test environments for running test cases and scenarios using Spring " +
+        "Tool Suite. Assisted with software testing",
+        ThirdJobDetail: "Participated in the deployment of QA processes using software tools and " +
+        "supporting technologies.",
         Skills: " MySQL · XAMPP · Spring Tool Suite · Selenium · Spring Boot · HTML · CSS · JavaScript · Java · GitHub"    
      },
      {
@@ -45,12 +73,19 @@ export const Slider = [
         Date: "Nov 2021 - Apr 2022",
         Company: "The Church of Jesus Christ of Latter-day Saints",
         Location: "Salt Lake City, Utah, United States",
-        FirstJobDetail: "Technical support on-site for employees in the Salt Lake City area and remotely across the United States and Canada.",
-        SecondJobDetail: "Managed IT service requests, events, incidents, and alerts using the Service Now ticketing system.",
-        ThirdJobDetail: "Enterprise Active Directory management using tools such as Microsoft Endpoint Manager, JAMF Pro Server for macOS, and ADUC.",
-        FourthJobDetail: "Technical support for server directories, network printers, Cisco VoIP phones, physical networks, integration of corporate email on smartphones such as iPhone and Android, and other network devices.",
-        FifthJobDetail: "Experience with Microsoft Active Directory, group policies, user profiles within Windows, imaging software, and file/security permissions.",
-        SixthJobDetail: "Experience supporting Microsoft cloud-based back-office systems including Azure AD, Exchange, SharePoint/OneDrive, Security & Compliance, ATP, InTune, AutoPilot, and Teams.",
+        FirstJobDetail: "Technical support on-site for employees in the Salt Lake City area and remotely " +
+        "across the United States and Canada.",
+        SecondJobDetail: "Managed IT service requests, events, incidents, and alerts using the Service Now " + 
+        "ticketing system.",
+        ThirdJobDetail: "Enterprise Active Directory management using tools such as Microsoft Endpoint " + 
+        "Manager, JAMF Pro Server for macOS, and ADUC.",
+        FourthJobDetail: "Technical support for server directories, network printers, Cisco VoIP phones, " +
+        "physical networks, integration of corporate email on smartphones such as iPhone and Android, and " +
+        "other network devices.",
+        FifthJobDetail: "Experience with Microsoft Active Directory, group policies, user profiles within " +
+        "Windows, imaging software, and file/security permissions.",
+        SixthJobDetail: "Experience supporting Microsoft cloud-based back-office systems including Azure " + 
+        "AD, Exchange, SharePoint/OneDrive, Security & Compliance, ATP, InTune, AutoPilot, and Teams.",
         Skills: ""    
      },
      {
@@ -59,15 +94,21 @@ export const Slider = [
         Date: "Ago 2021 - Nov 2021",
         Company: "Brigham Young University - Idaho",
         Location: "Rexburg, Idaho, United States",
-        FirstJobDetail: "Record and classify received Incidents and undertake an immediate effort to fix Computer issues.",
-        SecondJobDetail: "Monitor IT Technicians to ensure they reach their goals and the IT troubleshooting process is performed properly.",
-        ThirdJobDetail: "Responsible for assigning incidents within a group of 25 IT Technicians (IT Tier 2).",
-        FourthJobDetail: "Escalate unresolved incidents to external support within the University organization.",
+        FirstJobDetail: "Record and classify received Incidents and undertake an immediate effort to " +
+        "fix Computer issues.",
+        SecondJobDetail: "Monitor IT Technicians to ensure they reach their goals and the IT " + 
+        "troubleshooting process is performed properly.",
+        ThirdJobDetail: "Responsible for assigning incidents within a group of 25 IT Technicians " +
+        "(IT Tier 2).",
+        FourthJobDetail: "Escalate unresolved incidents to external support within the University " +
+        "organization.",
         FifthJobDetail: "Document troubleshooting steps and service restoration details.",
-        SixthJobDetail: "Investigate and diagnose IT Incidents to restore a failed IT Service as quickly as possible.",
-        SeventhJobDetail: "Restore a failed IT Service, repair hardware damage, and troubleshooting software issues as quickly as possible.",
-        EigthJobDetail: "Verify resolution with BYU-I students, faculty, Full-time employees, and resolve Incidents in the TeamDynamix tool.",
-        Skills: ""    
+        SixthJobDetail: "Investigate and diagnose IT Incidents to restore a failed IT Service as " +
+        "quickly as possible.",
+        SeventhJobDetail: "Restore a failed IT Service, repair hardware damage, and troubleshooting " +
+        "software issues as quickly as possible.",
+        EigthJobDetail: "Verify resolution with BYU-I students, faculty, Full-time employees, and " +
+        "resolve Incidents in the TeamDynamix tool.",   
      },
      {
         ExperienceTitle: "Work Experience",
@@ -83,7 +124,7 @@ export const Slider = [
         SixthJobDetail: "Troubleshooting Two-factor authentication methods.",
         SeventhJobDetail: "Imaging computers and installing appropriate software for faculty and student employees.",
         EigthJobDetail: "Performing data backups on Onedrive, mapping network drives, and updating Active Directory.",
-        NinethJobDetail: "Fixing any type of technical issues including WIFI connectivity, virtual machines, Office 365, AC Cloud, malware, upgrading operating systems, and software installation.",
-        Skills: ""    
+        NinethJobDetail: "Fixing any type of technical issues including WIFI connectivity, virtual machines, " +
+        "Office 365, AC Cloud, malware, upgrading operating systems, and software installation.",   
      }
 ];
