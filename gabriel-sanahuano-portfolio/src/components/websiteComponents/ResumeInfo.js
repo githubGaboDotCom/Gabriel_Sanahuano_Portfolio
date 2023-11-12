d771e39f90b318fb4a14ddedb35e18b39b1153f9
githubGaboDotCom <githubgabodotcom@gmail.com>
2023-11-11 23:57:05 -0700

    Almost done with the content of the resume window. Still finishing some minor details

--- gabriel-sanahuano-portfolio/src/components/websiteComponents/ResumeInfo.js
+++ gabriel-sanahuano-portfolio/src/components/websiteComponents/ResumeInfo.js
@@ -3,11 +3,21 @@ import '../styles/css/ResumeInfo.css';
 import { Slider } from './Slider';
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { faCircleArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 function ResumeInfo () {
 
     const [current, setCurrent] = useState(0);
-    const resumeSlider = Slider.length;
+    const sliderLength = Slider.length;
+
+    const nextSlide = () => {
+        setCurrent(current === sliderLength - 1 ? 0 : current + 1);
+    }
+
+    const previousSlide = () => {
+        setCurrent(current === 0 ? sliderLength - 1 : current - 1);
+    }
 
     return (
         <div className="ResumeParentContainer">
@@ -18,9 +28,47 @@ function ResumeInfo () {
                 <div className='TitleResumeContainer'>
                     <h2>Gabriel A. Sanahuano</h2>
                 </div>
-                <div>
-                    <FontAwesomeIcon icon="fa-solid fa-circle-arrow-left" />
-                    <FontAwesomeIcon icon="fa-solid fa-circle-arrow-right" />
+                <div className='sliderContainer'>
+                    <div className='leftArrowContainer'>
+                        <FontAwesomeIcon className='leftArrow' icon= {faCircleArrowLeft} onClick={previousSlide} />
+                    </div>
+                    <div className='EducationAndJobExperience'>
+                    {Slider.map((slide, index) => {
+                        return (
+                            <div className={index === current ? "slideActive" : "slide"} key={index}>
+                                {
+                                    index === current && (
+                                        <div>
+                                            <h3 className='EducationAndWorkTitle'>{slide.Title}</h3>
+                                            <div className='slideTextContainer'>
+                                                <div className='SubTitleAndOrg'>
+                                                    <h5 className='SubTitle'>{slide.SubTitle}</h5>
+                                                    <h5 className='Organization'>{slide.Organization}</h5>
+                                                </div>
+                                                <div className='DateAndLocation'>
+                                                    <h5 className='Date'>{slide.Date}</h5>
+                                                    <h5 className='Location'>{slide.Location}</h5>
+                                                </div>
+                                            </div>
+                                            <p className='BulletText'>{slide.FirstItem}</p>
+                                            <p className='BulletText'>{slide.SecondItem}</p>
+                                            <p className='BulletText'>{slide.ThirdItem}</p>
+                                            <p className='BulletText'>{slide.FourthItem}</p>
+                                            <p className='BulletText'>{slide.FifthItem}</p>
+                                            <p className='BulletText'>{slide.SixthItem}</p>
+                                            <p className='BulletText'>{slide.SeventhItem}</p>
+                                            <p className='BulletText'>{slide.EigthItem}</p>
+                                            <p className='BulletText'>{slide.NinethItem}</p>
+                                        </div>
+                                    )
+                                }
+                            </div>
+                        )
+                    })}
+                    </div>
+                    <div className='rightArrowContainer'>
+                        <FontAwesomeIcon className='rightArrow' icon={faCircleArrowRight} onClick={nextSlide} />
+                    </div>
                 </div>
             </motion.div>
         </div>
--- gabriel-sanahuano-portfolio/src/components/websiteComponents/Slider.js
+++ gabriel-sanahuano-portfolio/src/components/websiteComponents/Slider.js
@@ -1,11 +1,11 @@
 export const Slider = [
     {
-      EducationTitle: "Education",
-      Degree: "Bachelor of Science in Computer Science",
+      Title: "Education",
+      SubTitle: "Bachelor of Science in Computer Science",
       Date: "Jan 2019 - Dec 2022",
-      University: "Brigham Young University - Idaho",
+      Organization: "Brigham Young University - Idaho",
       Location: "Rexburg, Idaho, United States",
-      Coursework: "Introduction to Software Development, Object-Oriented Software Development, " +
+      FirstItem: "Coursework: Introduction to Software Development, Object-Oriented Software Development, " +
       "Fundamentals of Digital Systems,  Web Engineering I, Data Structures, Technical Documentation, " +
       "Database Design & Development, Software Design & Development, Web Engineering II, " +
       "Data Science Programming, Software Engineering III, Comp. Communication & Networks, " +
@@ -15,116 +15,116 @@ export const Slider = [
       "Data Analysis."
     },
     {
-      EducationTitle: "Education",
-      Degree: "Certificate in Machine Learning Fundamentals",
+      Title: "Education",
+      SubTitle: "Certificate in Machine Learning Fundamentals",
       Date: "Jul 2021 - Jul 2022",
-      University: "Brigham Young University - Idaho",
+      Organization: "Brigham Young University - Idaho",
       Location: "Rexburg, Idaho, United States",
-      FirstOutcome: "Use functions, data structures, and other programming constructs efficiently to process data.",
-      SecondOutcome: "Load data from various types of data sources, including files, databases, and remote services.",
-      ThirdOutcome: "Use data science libraries to perform analysis, produce charts, and prepare data for machine " +
+      FirstItem: "Use functions, data structures, and other programming constructs efficiently to process data.",
+      SecondItem: "Load data from various types of data sources, including files, databases, and remote services.",
+      ThirdItem: "Use data science libraries to perform analysis, produce charts, and prepare data for machine " +
       "learning algorithms.",
-      FourthOutcome: "Explain the approaches and appropriate applications of a broad set of machine learning algorithms.",
-      FifthOutcome: "Integrate and extend previously learned data science tools to analyze remote and distributed data " +
+      FourthItem: "Explain the approaches and appropriate applications of a broad set of machine learning algorithms.",
+      FifthItem: "Integrate and extend previously learned data science tools to analyze remote and distributed data " +
       "in business contexts.",
-      SixthOutcome: "Use mathematical software and/or calculators to solve a variety of applications in the Physical " +
+      SixthItem: "Use mathematical software and/or calculators to solve a variety of applications in the Physical " +
       "Sciences, Computer Science, or Economics.",
-      SeventhOutcome: "Gain proficiency in solving systems of linear equations using matrices, and demonstrate a working " +
+      SeventhItem: "Gain proficiency in solving systems of linear equations using matrices, and demonstrate a working " +
       "knowledge of the algebraic properties of matrices.",
-      EigthOutcome: "Acquire facility working with general vector spaces, linear transformations, coordinate vectors, " +
+      EigthItem: "Acquire facility working with general vector spaces, linear transformations, coordinate vectors, " +
       "and the changing of bases"
     },
     {
-       ExperienceTitle: "Work Experience",
-       JobTitle: "Software Test Engineer (Internship)",
+       Title: "Work Experience",
+       SubTitle: "Software Test Engineer (Internship)",
        Date: "Jan 2023 - Present",
-       Company: "The Church of Jesus Christ of Latter-day Saints",
+       Organization: "The Church of Jesus Christ of Latter-day Saints",
        Location: "Riverton, Utah, United States",
-       FirstJobDetail: "Contributed to over 170 user stories in Azure DevOps by conducting regression " +
+       FirstItem: "Contributed to over 170 user stories in Azure DevOps by conducting regression " +
        "testing, analyzing results, and submitting observations to the development team.",
-       SecondJobDetail: "Participated in design reviews to identify inconsistencies//issues in product " +
+       SecondItem: "Participated in design reviews to identify inconsistencies//issues in product " +
        "requirements by interacting with business analysts and developers during the sprint cycle.",
-       ThirdJobDetail: "Performed integration, End-to-End, performance, regression, sanity, and smoke " +
+       ThirdItem: "Performed integration, End-to-End, performance, regression, sanity, and smoke " +
        "testing on Mobile and Web applications by developing negative and positive test cases according " +
        "to business requirements.",
-       FourthJobDetail: "Created automated test cases using Cypress and executed SQL queries for data " +
+       FourthItem: "Created automated test cases using Cypress and executed SQL queries for data " +
        "validation and debugging.",
-       FifthJobDetail: "Assisted with continuous integration and deployment practices in Azure DevOps " +
+       FifthItem: "Assisted with continuous integration and deployment practices in Azure DevOps " +
        "using an Agile Software Development process.",
        Skills: "Software Testing Life Cycle (STLC) · QA Automation · Test Scripts · JavaScript · SQL · " + 
        "Microsoft Dynamics 365 · Azure DevOps · Git · Visual Studio IDE · Cypress · Agile Software Development."    
     },
     {
-        ExperienceTitle: "Work Experience",
-        JobTitle: "Software Engineer (Internship)",
+        Title: "Work Experience",
+        SubTitle: "Software Engineer (Internship)",
         Date: "Sep 2022 - Dec 2022",
-        Company: "We Care Insurance",
+        Organization: "We Care Insurance",
         Location: "Omaha, Nebraska, United States",
-        FirstJobDetail: "Designed, created, and executed automation scripts using Java and Selenium.",
-        SecondJobDetail: "Configured test environments for running test cases and scenarios using Spring " +
+        FirstItem: "Designed, created, and executed automation scripts using Java and Selenium.",
+        SecondItem: "Configured test environments for running test cases and scenarios using Spring " +
         "Tool Suite. Assisted with software testing",
-        ThirdJobDetail: "Participated in the deployment of QA processes using software tools and " +
+        ThirdItem: "Participated in the deployment of QA processes using software tools and " +
         "supporting technologies.",
         Skills: " MySQL · XAMPP · Spring Tool Suite · Selenium · Spring Boot · HTML · CSS · JavaScript · Java · GitHub"    
      },
      {
-        ExperienceTitle: "Work Experience",
-        JobTitle: "Technical Support Representative (Internship)",
+        Title: "Work Experience",
+        SubTitle: "Technical Support Representative (Internship)",
         Date: "Nov 2021 - Apr 2022",
-        Company: "The Church of Jesus Christ of Latter-day Saints",
+        Organization: "The Church of Jesus Christ of Latter-day Saints",
         Location: "Salt Lake City, Utah, United States",
-        FirstJobDetail: "Technical support on-site for employees in the Salt Lake City area and remotely " +
+        FirstItem: "Technical support on-site for employees in the Salt Lake City area and remotely " +
         "across the United States and Canada.",
-        SecondJobDetail: "Managed IT service requests, events, incidents, and alerts using the Service Now " + 
+        SecondItem: "Managed IT service requests, events, incidents, and alerts using the Service Now " + 
         "ticketing system.",
-        ThirdJobDetail: "Enterprise Active Directory management using tools such as Microsoft Endpoint " + 
+        ThirdItem: "Enterprise Active Directory management using tools such as Microsoft Endpoint " + 
         "Manager, JAMF Pro Server for macOS, and ADUC.",
-        FourthJobDetail: "Technical support for server directories, network printers, Cisco VoIP phones, " +
+        FourthItem: "Technical support for server directories, network printers, Cisco VoIP phones, " +
         "physical networks, integration of corporate email on smartphones such as iPhone and Android, and " +
         "other network devices.",
-        FifthJobDetail: "Experience with Microsoft Active Directory, group policies, user profiles within " +
+        FifthItem: "Experience with Microsoft Active Directory, group policies, user profiles within " +
         "Windows, imaging software, and file/security permissions.",
-        SixthJobDetail: "Experience supporting Microsoft cloud-based back-office systems including Azure " + 
+        SixthItem: "Experience supporting Microsoft cloud-based back-office systems including Azure " + 
         "AD, Exchange, SharePoint/OneDrive, Security & Compliance, ATP, InTune, AutoPilot, and Teams.",
         Skills: ""    
      },
      {
-        ExperienceTitle: "Work Experience",
-        JobTitle: "IT Incidents Lead Technician (Advanced Technical Suport)",
+        Title: "Work Experience",
+        SubTitle: "IT Incidents Lead Technician (Advanced Technical Suport)",
         Date: "Ago 2021 - Nov 2021",
-        Company: "Brigham Young University - Idaho",
+        Organization: "Brigham Young University - Idaho",
         Location: "Rexburg, Idaho, United States",
-        FirstJobDetail: "Record and classify received Incidents and undertake an immediate effort to " +
+        FirstItem: "Record and classify received Incidents and undertake an immediate effort to " +
         "fix Computer issues.",
-        SecondJobDetail: "Monitor IT Technicians to ensure they reach their goals and the IT " + 
+        SecondItem: "Monitor IT Technicians to ensure they reach their goals and the IT " + 
         "troubleshooting process is performed properly.",
-        ThirdJobDetail: "Responsible for assigning incidents within a group of 25 IT Technicians " +
+        ThirdItem: "Responsible for assigning incidents within a group of 25 IT Technicians " +
         "(IT Tier 2).",
-        FourthJobDetail: "Escalate unresolved incidents to external support within the University " +
+        FourthItem: "Escalate unresolved incidents to external support within the University " +
         "organization.",
-        FifthJobDetail: "Document troubleshooting steps and service restoration details.",
-        SixthJobDetail: "Investigate and diagnose IT Incidents to restore a failed IT Service as " +
+        FifthItem: "Document troubleshooting steps and service restoration details.",
+        SixthItem: "Investigate and diagnose IT Incidents to restore a failed IT Service as " +
         "quickly as possible.",
-        SeventhJobDetail: "Restore a failed IT Service, repair hardware damage, and troubleshooting " +
+        SeventhItem: "Restore a failed IT Service, repair hardware damage, and troubleshooting " +
         "software issues as quickly as possible.",
-        EigthJobDetail: "Verify resolution with BYU-I students, faculty, Full-time employees, and " +
+        EigthItem: "Verify resolution with BYU-I students, faculty, Full-time employees, and " +
         "resolve Incidents in the TeamDynamix tool.",   
      },
      {
-        ExperienceTitle: "Work Experience",
-        JobTitle: "IT Support Technician",
+        Title: "Work Experience",
+        SubTitle: "IT Support Technician",
         Date: "Jul 2020 - Ago 2021",
-        Company: "Brigham Young University - Idaho",
+        Organization: "Brigham Young University - Idaho",
         Location: "Rexburg, Idaho, United States",
-        FirstJobDetail: "IT troubleshooting & problem-solving.",
-        SecondJobDetail: "Computer hardware systems & components.",
-        ThirdJobDetail: "Answering phone calls and remote technical support.",
-        FourthJobDetail: "LAN & WAN Connectivity & Maintenance.",
-        FifthJobDetail: "Installing, repairing, maintaining, and upgrading Windows and Mac operating systems.",
-        SixthJobDetail: "Troubleshooting Two-factor authentication methods.",
-        SeventhJobDetail: "Imaging computers and installing appropriate software for faculty and student employees.",
-        EigthJobDetail: "Performing data backups on Onedrive, mapping network drives, and updating Active Directory.",
-        NinethJobDetail: "Fixing any type of technical issues including WIFI connectivity, virtual machines, " +
+        FirstItem: "IT troubleshooting & problem-solving.",
+        SecondItem: "Computer hardware systems & components.",
+        ThirdItem: "Answering phone calls and remote technical support.",
+        FourthItem: "LAN & WAN Connectivity & Maintenance.",
+        FifthItem: "Installing, repairing, maintaining, and upgrading Windows and Mac operating systems.",
+        SixthItem: "Troubleshooting Two-factor authentication methods.",
+        SeventhItem: "Imaging computers and installing appropriate software for faculty and student employees.",
+        EigthItem: "Performing data backups on Onedrive, mapping network drives, and updating Active Directory.",
+        NinethItem: "Fixing any type of technical issues including WIFI connectivity, virtual machines, " +
         "Office 365, AC Cloud, malware, upgrading operating systems, and software installation.",   
      }
 ];
