bc4e444505c54d62d25053926f3033d3ccc85ba5
githubGaboDotCom <githubgabodotcom@gmail.com>
2023-11-12 01:13:16 -0700

    Finished adding the content for the resume window

--- gabriel-sanahuano-portfolio/src/components/websiteComponents/Slider.js
+++ gabriel-sanahuano-portfolio/src/components/websiteComponents/Slider.js
@@ -20,19 +20,16 @@ export const Slider = [
       Date: "Jul 2021 - Jul 2022",
       Organization: "Brigham Young University - Idaho",
       Location: "Rexburg, Idaho, United States",
-      FirstItem: "Use functions, data structures, and other programming constructs efficiently to process data.",
-      SecondItem: "Load data from various types of data sources, including files, databases, and remote services.",
-      ThirdItem: "Use data science libraries to perform analysis, produce charts, and prepare data for machine " +
+      FirstItem: "\u2022 Use functions, data structures, and other programming constructs efficiently to process data.",
+      SecondItem: "\u2022 Load data from various types of data sources, including files, databases, and remote services.",
+      ThirdItem: "\u2022 Use data science libraries to perform analysis, produce charts, and prepare data for machine " +
       "learning algorithms.",
-      FourthItem: "Explain the approaches and appropriate applications of a broad set of machine learning algorithms.",
-      FifthItem: "Integrate and extend previously learned data science tools to analyze remote and distributed data " +
+      FourthItem: "\u2022 Integrate and extend previously learned data science tools to analyze remote and distributed data " +
       "in business contexts.",
-      SixthItem: "Use mathematical software and/or calculators to solve a variety of applications in the Physical " +
+      FifthItem: "\u2022 Use mathematical software and/or calculators to solve a variety of applications in the Physical " +
       "Sciences, Computer Science, or Economics.",
-      SeventhItem: "Gain proficiency in solving systems of linear equations using matrices, and demonstrate a working " +
-      "knowledge of the algebraic properties of matrices.",
-      EigthItem: "Acquire facility working with general vector spaces, linear transformations, coordinate vectors, " +
-      "and the changing of bases"
+      SixthItem: "\u2022 Gain proficiency in solving systems of linear equations using matrices, and demonstrate a working " +
+      "knowledge of the algebraic properties of matrices."
     },
     {
        Title: "Work Experience",
@@ -40,19 +37,19 @@ export const Slider = [
        Date: "Jan 2023 - Present",
        Organization: "The Church of Jesus Christ of Latter-day Saints",
        Location: "Riverton, Utah, United States",
-       FirstItem: "Contributed to over 170 user stories in Azure DevOps by conducting regression " +
+       FirstItem: "\u2022 Contributed to over 170 user stories in Azure DevOps by conducting regression " +
        "testing, analyzing results, and submitting observations to the development team.",
-       SecondItem: "Participated in design reviews to identify inconsistencies//issues in product " +
+       SecondItem: "\u2022 Participated in design reviews to identify inconsistencies//issues in product " +
        "requirements by interacting with business analysts and developers during the sprint cycle.",
-       ThirdItem: "Performed integration, End-to-End, performance, regression, sanity, and smoke " +
+       ThirdItem: "\u2022 Performed integration, End-to-End, performance, regression, sanity, and smoke " +
        "testing on Mobile and Web applications by developing negative and positive test cases according " +
        "to business requirements.",
-       FourthItem: "Created automated test cases using Cypress and executed SQL queries for data " +
+       FourthItem: "\u2022 Created automated test cases using Cypress and executed SQL queries for data " +
        "validation and debugging.",
-       FifthItem: "Assisted with continuous integration and deployment practices in Azure DevOps " +
+       FifthItem: "\u2022 Assisted with continuous integration and deployment practices in Azure DevOps " +
        "using an Agile Software Development process.",
-       Skills: "Software Testing Life Cycle (STLC) · QA Automation · Test Scripts · JavaScript · SQL · " + 
-       "Microsoft Dynamics 365 · Azure DevOps · Git · Visual Studio IDE · Cypress · Agile Software Development."    
+       SixthItem: "\u2022 Skills: Software Testing Life Cycle (STLC) · QA Automation · Test Scripts · JavaScript · SQL · " + 
+       "Microsoft Dynamics 365 · Azure DevOps · Git · Cypress · Agile Software Development."    
     },
     {
         Title: "Work Experience",
@@ -60,12 +57,12 @@ export const Slider = [
         Date: "Sep 2022 - Dec 2022",
         Organization: "We Care Insurance",
         Location: "Omaha, Nebraska, United States",
-        FirstItem: "Designed, created, and executed automation scripts using Java and Selenium.",
-        SecondItem: "Configured test environments for running test cases and scenarios using Spring " +
+        FirstItem: "\u2022 Designed, created, and executed automation scripts using Java and Selenium.",
+        SecondItem: "\u2022 Configured test environments for running test cases and scenarios using Spring " +
         "Tool Suite. Assisted with software testing",
-        ThirdItem: "Participated in the deployment of QA processes using software tools and " +
+        ThirdItem: "\u2022 Participated in the deployment of QA processes using software tools and " +
         "supporting technologies.",
-        Skills: " MySQL · XAMPP · Spring Tool Suite · Selenium · Spring Boot · HTML · CSS · JavaScript · Java · GitHub"    
+        FourthItem: "\u2022 Skills: MySQL · XAMPP · Spring Tool Suite · Selenium · Spring Boot · HTML · CSS · JavaScript · Java · GitHub"    
      },
      {
         Title: "Work Experience",
@@ -73,20 +70,19 @@ export const Slider = [
         Date: "Nov 2021 - Apr 2022",
         Organization: "The Church of Jesus Christ of Latter-day Saints",
         Location: "Salt Lake City, Utah, United States",
-        FirstItem: "Technical support on-site for employees in the Salt Lake City area and remotely " +
+        FirstItem: "\u2022 Technical support on-site for employees in the Salt Lake City area and remotely " +
         "across the United States and Canada.",
-        SecondItem: "Managed IT service requests, events, incidents, and alerts using the Service Now " + 
+        SecondItem: "\u2022 Managed IT service requests, events, incidents, and alerts using the Service Now " + 
         "ticketing system.",
-        ThirdItem: "Enterprise Active Directory management using tools such as Microsoft Endpoint " + 
+        ThirdItem: "\u2022 Enterprise Active Directory management using tools such as Microsoft Endpoint " + 
         "Manager, JAMF Pro Server for macOS, and ADUC.",
-        FourthItem: "Technical support for server directories, network printers, Cisco VoIP phones, " +
+        FourthItem: "\u2022 Technical support for server directories, network printers, Cisco VoIP phones, " +
         "physical networks, integration of corporate email on smartphones such as iPhone and Android, and " +
         "other network devices.",
-        FifthItem: "Experience with Microsoft Active Directory, group policies, user profiles within " +
+        FifthItem: "\u2022 Experience with Microsoft Active Directory, group policies, user profiles within " +
         "Windows, imaging software, and file/security permissions.",
         SixthItem: "Experience supporting Microsoft cloud-based back-office systems including Azure " + 
-        "AD, Exchange, SharePoint/OneDrive, Security & Compliance, ATP, InTune, AutoPilot, and Teams.",
-        Skills: ""    
+        "AD, SharePoint/OneDrive, Security & Compliance, ATP, InTune, AutoPilot, and Teams."    
      },
      {
         Title: "Work Experience",
@@ -94,21 +90,17 @@ export const Slider = [
         Date: "Ago 2021 - Nov 2021",
         Organization: "Brigham Young University - Idaho",
         Location: "Rexburg, Idaho, United States",
-        FirstItem: "Record and classify received Incidents and undertake an immediate effort to " +
+        FirstItem: "\u2022 Record and classify received Incidents and undertake an immediate effort to " +
         "fix Computer issues.",
-        SecondItem: "Monitor IT Technicians to ensure they reach their goals and the IT " + 
+        SecondItem: "\u2022 Monitor IT Technicians to ensure they reach their goals and the IT " + 
         "troubleshooting process is performed properly.",
-        ThirdItem: "Responsible for assigning incidents within a group of 25 IT Technicians " +
+        ThirdItem: "\u2022 Responsible for assigning incidents within a group of 25 IT Technicians " +
         "(IT Tier 2).",
-        FourthItem: "Escalate unresolved incidents to external support within the University " +
-        "organization.",
-        FifthItem: "Document troubleshooting steps and service restoration details.",
-        SixthItem: "Investigate and diagnose IT Incidents to restore a failed IT Service as " +
-        "quickly as possible.",
-        SeventhItem: "Restore a failed IT Service, repair hardware damage, and troubleshooting " +
+        FourthItem: "\u2022 Restore a failed IT Service, repair hardware damage, and troubleshooting " +
         "software issues as quickly as possible.",
-        EigthItem: "Verify resolution with BYU-I students, faculty, Full-time employees, and " +
-        "resolve Incidents in the TeamDynamix tool.",   
+        FifthItem: "\u2022 Document troubleshooting steps and service restoration details.",
+        SixthItem: "\u2022 Investigate and diagnose IT Incidents to restore a failed IT Service as " +
+        "quickly as possible."  
      },
      {
         Title: "Work Experience",
@@ -116,15 +108,13 @@ export const Slider = [
         Date: "Jul 2020 - Ago 2021",
         Organization: "Brigham Young University - Idaho",
         Location: "Rexburg, Idaho, United States",
-        FirstItem: "IT troubleshooting & problem-solving.",
-        SecondItem: "Computer hardware systems & components.",
-        ThirdItem: "Answering phone calls and remote technical support.",
-        FourthItem: "LAN & WAN Connectivity & Maintenance.",
-        FifthItem: "Installing, repairing, maintaining, and upgrading Windows and Mac operating systems.",
-        SixthItem: "Troubleshooting Two-factor authentication methods.",
-        SeventhItem: "Imaging computers and installing appropriate software for faculty and student employees.",
-        EigthItem: "Performing data backups on Onedrive, mapping network drives, and updating Active Directory.",
-        NinethItem: "Fixing any type of technical issues including WIFI connectivity, virtual machines, " +
-        "Office 365, AC Cloud, malware, upgrading operating systems, and software installation.",   
+        FirstItem: "\u2022 IT troubleshooting & problem-solving.",
+        SecondItem: "\u2022 Performing data backups on Onedrive, mapping network drives, and updating Active Directory.",
+        ThirdItem: "\u2022 Answering phone calls and remote technical support.",
+        FourthItem: "\u2022 LAN & WAN Connectivity & Maintenance.",
+        FifthItem: "\u2022 Installing, repairing, maintaining, and upgrading Windows and Mac operating systems.",
+        SixthItem: "\u2022 Fixing any type of technical issues including WIFI connectivity, virtual machines, " +
+        "Office 365, AC Cloud, malware, upgrading operating systems, and software installation.",
+        SeventhItem: "\u2022 Imaging computers and installing appropriate software for faculty and student employees." 
      }
 ];
