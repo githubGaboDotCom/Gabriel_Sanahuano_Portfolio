f6d6822d182a3cbd92c1bfb5aba4f592b1305ed5
githubGaboDotCom <githubgabodotcom@gmail.com>
2023-11-14 23:23:46 -0700

    I started working on the MySkills.js window. I am adding the initial code to start developing it

--- gabriel-sanahuano-portfolio/src/components/websiteComponents/MySkills.js
+++ gabriel-sanahuano-portfolio/src/components/websiteComponents/MySkills.js
@@ -2,13 +2,46 @@ import { motion } from "framer-motion";
 import '../styles/css/MySkills.css';
 
 function MySkills () {
+
+    const draw = {
+        hidden: {pathLength: 0, opacity: 0},
+        visible: (i) => {
+            const delay = 1 + i * 0.5;
+            return {
+                pathLength: 0.8,
+                opacity: 0.7,
+                transition: {
+                    pathLength: {delay, type: 'spring', duration: 3.5, bounce: 0},
+                    opacity: {delay, duration: 0.01}
+                }
+            }
+        }
+    };
+
     return (
         <div className="MySkillsParentContainer">
             <motion.div className = 'MySkillsMainContainer' 
             initial = {{opacity: 0, scale: 0.9}}
             animate = {{opacity: 1, scale: 1}}
             transition = {{duration: 0.5, delay: 0.5, ease: [0, 0.71, 0.2, 1.01]}}>
-                <h1>This is the Skills window.</h1>
+                <motion.svg
+                width='600'
+                height='600'
+                viewBox='0 0 600 600'
+                initial='hidden'
+                animate='visible'>
+                    <motion.circle 
+                    cx='100'
+                    cy='100'
+                    r='80'
+                    stroke='#1aff1a'
+                    fill='rgb(55, 101, 40, 0.5)'
+                    stroke-width='15'
+                    variants={draw}
+                    custom={1}/>
+                        <motion.text x='72' y='110' fill='white' fontSize='40px' opacity='0.7'>80%</motion.text>
+                        <motion.text x='80' y='150' fill='white' fontSize='23px'>C++</motion.text>
+                </motion.svg>
             </motion.div>
         </div>
     );
