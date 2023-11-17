6519cb09a7b9cc55d36d61b1addf60a311526e3c
githubGaboDotCom <githubgabodotcom@gmail.com>
2023-11-16 23:15:44 -0700

    Working on the Skills window

--- gabriel-sanahuano-portfolio/src/components/websiteComponents/MySkills.js
+++ gabriel-sanahuano-portfolio/src/components/websiteComponents/MySkills.js
@@ -1,5 +1,8 @@
 import { motion } from "framer-motion";
 import '../styles/css/MySkills.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { faCircleArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 function MySkills () {
 
@@ -27,25 +30,35 @@ function MySkills () {
                 <div className="TitleSkillsContainer">
                     <h2>My Skills</h2>
                 </div>
-                <h3 className="SubTitleMySkillsContainer">Programming Skills</h3>
-                <motion.svg
-                width='600'
-                height='600'
-                viewBox='0 0 600 600'
-                initial='hidden'
-                animate='visible'>
-                    <motion.circle 
-                    cx='100'
-                    cy='100'
-                    r='80'
-                    stroke='#1aff1a'
-                    fill='rgb(55, 101, 40, 0.5)'
-                    stroke-width='15'
-                    variants={draw}
-                    custom={1}/>
-                        <motion.text x='72' y='110' fill='white' fontSize='40px' opacity='0.7'>80%</motion.text>
-                        <motion.text x='80' y='150' fill='white' fontSize='23px'>C++</motion.text>
-                </motion.svg>
+                <div className="MySkillsSliderContainer">
+                    <div className='leftArrowContainer'>
+                        <FontAwesomeIcon className='leftArrow' icon= {faCircleArrowLeft} />
+                    </div>
+                    <div className="MySkillsContentSliderContainer">
+                        <h3 className="SubTitleMySkillsContainer">Programming Skills</h3>
+                        <motion.svg
+                        width='200'
+                        height='200'
+                        viewBox='0 0 200 200'
+                        initial='hidden'
+                        animate='visible'>
+                            <motion.circle 
+                            cx='100'
+                            cy='100'
+                            r='80'
+                            stroke='#1aff1a'
+                            fill='rgb(55, 101, 40, 0.5)'
+                            stroke-width='15'
+                            variants={draw}
+                            custom={1}/>
+                                <motion.text x='72' y='110' fill='white' fontSize='40px' opacity='0.7'>80%</motion.text>
+                                <motion.text x='80' y='150' fill='white' fontSize='23px'>C++</motion.text>
+                        </motion.svg>
+                    </div>
+                    <div className='rightArrowContainer'>
+                        <FontAwesomeIcon className='rightArrow' icon={faCircleArrowRight} />
+                    </div>
+                </div>
             </motion.div>
         </div>
     );
