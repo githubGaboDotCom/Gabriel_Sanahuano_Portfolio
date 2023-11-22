ea23a90bedcb0beba41998bd6f0e1f2944fd370a
githubGaboDotCom <githubgabodotcom@gmail.com>
2023-11-21 23:32:29 -0700

    Starting work on the Projects window

--- gabriel-sanahuano-portfolio/src/components/websiteComponents/MyProjects.js
+++ gabriel-sanahuano-portfolio/src/components/websiteComponents/MyProjects.js
@@ -8,7 +8,9 @@ function MyProjects () {
             initial = {{opacity: 0, scale: 0.9}}
             animate = {{opacity: 1, scale: 1}}
             transition = {{duration: 0.5, delay: 0.5, ease: [0, 0.71, 0.2, 1.01]}}>
-                <h1>This is the Projects window.</h1>
+                <div className="ProjectsTitleContainer">
+                    <h2>My Projects</h2>
+                </div>
             </motion.div>
         </div>
     );
