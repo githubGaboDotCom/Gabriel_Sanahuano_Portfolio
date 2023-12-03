bc0d8ce9bb3550be51911fb21fb8c0873bc9ee96
githubGaboDotCom <githubgabodotcom@gmail.com>
2023-12-02 23:40:54 -0700

    Modified MyProjectSlider.js file

--- gabriel-sanahuano-portfolio/src/components/websiteComponents/MyProjectsSlider.js
+++ gabriel-sanahuano-portfolio/src/components/websiteComponents/MyProjectsSlider.js
@@ -26,11 +26,10 @@ export const MyProjectsSlider = [
     {
         AppName: 'Time Tracking App',
         WebUrl: 'https://www.youtube.com/embed/9ivLkYlOeOI?si=IgYrUPyqStmCVVoD',
-        AppDescription: 'This app provides users with a way and manage their time in a more ' +
-        'efficient way. You can create an account and login to record all your daily activities ' +
-        'and the time you spent doing them. You can also see monthly and annual statistics ' +
-        'on the activities that consume most of your time, and use the app to set goals and be ' +
-        'more productive throughout your day. ',
+        AppDescription: 'This app provides users with a way to manage their time more efficiently. ' +
+        'You can create an account and log in to record all your daily activities and the time you ' +
+        'spend doing them. You can also see monthly and annual statistics on the activities that consume ' +
+        'most of your time, and use the app to set goals and be more productive throughout your day.',
         ProjectSkills: 'Skills: HTML, CSS, JavaScript.',
         AppWebURL: 'https://github.com/githubGaboDotCom/TimeTrackingApp-Project',
         GithubRepositoryLink: 'https://github.com/githubGaboDotCom/TimeTrackingApp-Project',
@@ -49,12 +48,15 @@ export const MyProjectsSlider = [
         TextAppLink: 'Link to download the app in the app store.'
     },
     {
-        AppName: 'Docker Streamlit App',
+        AppName: 'Analytics Deployment App with Docker and Streamlit',
         WebUrl: 'https://www.youtube.com/embed/9ivLkYlOeOI?si=IgYrUPyqStmCVVoD',
-        AppDescription: 'To Be Determined',
-        ProjectSkills: 'Skills: HTML, CSS, JavaScript.',
+        AppDescription: 'This project consists of a study to understand the behavior of Chipotle consumers. I ' +
+        'used a Safegraph database to access information about people who have visited Chipotle facilities' +
+        'in the last four years. This study helps us know the popular brands that Chipotle customers prefer ' +
+        'to go to often, either on the same day or in the same month of visiting Chipotle.',
+        ProjectSkills: 'Skills: Pyspark, SparkSQL, Python, Docker, Databricks, Streamlit',
         AppWebURL: 'https://github.com/githubGaboDotCom/Docker-Streamlit_App_with_Heroku',
         GithubRepositoryLink: 'https://github.com/githubGaboDotCom/Docker-Streamlit_App_with_Heroku',
-        TextAppLink: 'Click here to go to the Docker Streamlit App'
+        TextAppLink: 'Click here to go to the Analytics Deployment App'
     }
 ];
